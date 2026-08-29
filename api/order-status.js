aa094abe1097467451ee0fec322b65ba5876b2e8
opheliayeboahbanso-debug <opheliayeboahbanso@gmail.com>
2026-08-29 18:55:37 +0000

    Update order-status.js

--- api/order-status.js
+++ api/order-status.js
@@ -1,13 +1,6 @@
 export default async function handler(req, res) {
-
-if (req.method !== "GET") {
-return res.status(405).json({
-error: "Method not allowed"
-});
-}
-
+if (req.method !== "GET") { return res.status(405).json({ error: "Method not allowed" }); }
 try {
-
 const { reference } = req.query;
 
 if (!reference) {
@@ -83,28 +76,3 @@ return res.status(200).json({
 
   data:
     bossResult.data || bossResult
-
-});
-
-} catch (error) {
-
-console.error(
-  "ORDER STATUS ERROR:",
-  error
-);
-
-return res.status(500).json({
-
-  success: false,
-
-  error:
-    "Unable to check order",
-
-  details:
-    error.message
-
-});
-
-}
-
-}
